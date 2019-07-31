0ae2a5d717f8091352d85184bb9e56388b5667f5
czy0729 <402731062@qq.com>
2019-07-31 09:57:18 +0800

    - [x] 照片详情 /detail [1] - [x] 文章详情 /article [1]

---
+++ src/pages/article/ds.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: czy0729
+ * @Date: 2019-07-31 09:48:19
+ * @Last Modified by: czy0729
+ * @Last Modified time: 2019-07-31 09:50:49
+ */
+export const rootCls = 'page-article'
+
+export const infoDS = [
+  {
+    label: '户型',
+    value: '两室'
+  },
+  {
+    label: '使用面积',
+    value: '85平'
+  },
+  {
+    label: '灯具花费',
+    value: '2万元'
+  },
+  {
+    label: '位置',
+    value: '广州'
+  }
+]
---
+++ src/pages/detail/ds.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: czy0729
+ * @Date: 2019-07-31 09:25:35
+ * @Last Modified by: czy0729
+ * @Last Modified time: 2019-07-31 09:34:31
+ */
+// eslint-disable-next-line import/prefer-default-export
+export const rootCls = 'page-detail'
